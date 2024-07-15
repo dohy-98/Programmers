bb20d2df2e32f69dfc04dca8b34381739a917eab
dohy-98 <akdmatla123@gmail.com>
2024-07-15 19:43:55 +0900

    [Bronze III] Title: 최소, 최대, Time: 348 ms, Memory: 153832 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,8 +1,3 @@
-N = int(input().strip())
-rep = []
-numbers = list(map(int, input().strip().split()))
-
-rep.append(min(numbers))
-rep.append(max(numbers))
-for i in range(len(rep)) :
-    print(rep[i], end=" ")
+cnt = int(input())
+numbers = list(map(int, input().split()))
+print(min(numbers),max(numbers))
