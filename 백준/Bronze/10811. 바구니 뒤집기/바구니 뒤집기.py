9ced7ac8f941e0254b8d6454ca4d60177b8459e2
dohy-98 <akdmatla123@gmail.com>
2024-07-15 19:06:12 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,18 +1,12 @@
 N,M = map(int,input().strip().split())
-rep = []
-num = 0
-if 1 <= N <= 100 and 1 <= M <= 100 :
-    for i in range(1,1+N) :
-        rep.append(str(i))
 
-while True :
-    i,j = map(int,input().strip().split())
-    if 1 <= i <= j <= N :
-        slice_part = rep[i-1:j]
-        slice_part.reverse()
-        rep[i-1:j] = slice_part
-        num += 1
-    if num == M:
-        break
-result = " ".join(rep)
-print(result)
+basket = [n for n in range(1,N+1)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    temp = basket[i-1:j]
+    temp.reverse()
+    basket[i-1:j] = temp 
+
+for i in range(N):
+    print(basket[i], end=" ")
