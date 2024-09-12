25fdd9b5264a4e4d9da21ba20576cdc59182623e
dohy-98 <akdmatla123@gmail.com>
2024-09-12 09:23:03 +0900

    [Bronze I] Title: 최소공배수, Time: 4688 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for _ in range(N) :
+    a = list(map(int,input().split()))
+    rep = []
+
+    for i in range(1,min(a)+1) :
+        if a[0] % i == 0 and a[1] % i == 0 :
+            rep.append(i)
+
+    print(a[0]*a[1]//max(rep))
